45b355d5a98b298dea0b68efc3dfdda1bf0f6652
Karel Mundnich <mundnich@usc.edu>
2019-03-02 15:14:06 -0800

    TaskA results for tSTE with logistic noise

--- simulations_logistic.jl
+++ simulations_logistic.jl
@@ -24,7 +24,7 @@ end
 
 function mock_args() # For debugging
 	args = Dict{String,Any}()
-	args["data"] = "./data/TaskB.csv"
+	args["data"] = "./data/TaskA.csv"
 
 	return args
 end
@@ -234,14 +234,14 @@ function main()
 	Random.seed!(4)
 	tSTE(args, data, experiment)
 
-	Random.seed!(4)
-	STE(args, data, experiment)
+	#Random.seed!(4)
+	#STE(args, data, experiment)
 
-	Random.seed!(4)
-	GNMDS(args, data, experiment)
+	#Random.seed!(4)
+	#GNMDS(args, data, experiment)
 
-	Random.seed!(4)
-	CKL(args, data, experiment)
+	#Random.seed!(4)
+	#CKL(args, data, experiment)
 
 
 end
