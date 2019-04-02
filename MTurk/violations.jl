62ff9960cf4429f080b14be323548639adbec6f9
Karel Mundnich <mundnich@usc.edu>
2019-04-02 11:20:00 -0700

    Added file

---
+++ MTurk/violations.jl
@@ -0,0 +1,12 @@
+using JLD
+using MAT
+using Dates
+using Random
+using Plots; gr()
+using DataFrames
+using Statistics
+using LinearAlgebra
+
+include("TripletEmbeddings.jl/src/Embeddings.jl")
+include("Mturk/MTurk.jl")
+
